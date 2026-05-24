217300d9c970c6d0f84ee0e6b8f5df8176a1ee56
ntshabelebenjamin-ops <ntshabelebenjamin@gmail.com>
2026-05-24 19:08:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
 
+st.title("Institutional Analytics Dashboard")
+
+st.write("Welcome to the dashboard")
+
+df = pd.read_excel("Faculty Enrolment.xlsx")
+
+st.write(df.head())
